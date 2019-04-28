8a9ca84331a111f6853217ed7e089267c8b3d60e
kthays <kthays23@gmail.com>
2019-04-28 12:00:31 -0600

    Run alert when youtube page is loaded

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,11 @@
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log("The color is green.");
-    });
-    
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
+function isYoutubeURL(url)
+{
+    return url && url.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/gm);
+}
+
+// https://stackoverflow.com/a/40424806
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (isYoutubeURL(changeInfo.url)) {
+        chrome.tabs.executeScript(null, {file: 'buttonadder.js'});
+    }
 });
