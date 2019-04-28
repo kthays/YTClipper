b411a79b4d48e7eaae2eaeba56e096800a92a4f9
kthays <kthays23@gmail.com>
2019-04-28 17:42:15 -0600

    Added record button to player controls

--- extension/buttonadder.js
+++ extension/buttonadder.js
@@ -1,1 +1,42 @@
-alert('BUTTON ADDER');
+function addButton() {
+
+    const playerControls = document.getElementsByClassName('ytp-left-controls')[0];
+    if (!playerControls) {
+        console.log('YTClipper failed to get player controls');
+        return;
+    }
+    
+    const recordButtonID = 'YTClipper';
+    if (document.getElementById(recordButtonID)) return; // We've already added the button
+
+    // Make a custom button by copying the play button
+    const playButton = playerControls.getElementsByClassName('ytp-play-button')[0];
+    const recordButton = playButton.cloneNode(true);
+    recordButton.id = recordButtonID;
+    recordButton.setAttribute('title', 'Record');
+    recordButton.setAttribute('aria-label', 'Record');
+
+    // Add the record button to the right of the timestamp controls
+    playButton.parentElement.appendChild(recordButton);
+
+    // Replace the triangle path with circles...
+    const buttonPath = recordButton.getElementsByTagName('svg')[0];
+    buttonPath.removeChild(buttonPath.children[1]); // Remove the triangle path
+    
+    // Add the circle paths
+    const circleOuter = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    circleOuter.setAttributeNS(null, 'cx', 15.25);
+    circleOuter.setAttributeNS(null, 'cy', 18);
+    circleOuter.setAttributeNS(null, 'r', 6);
+    circleOuter.setAttributeNS(null, 'style', 'fill: none; stroke: white; stroke-width: 1.5px');
+    buttonPath.appendChild(circleOuter);
+    
+    const circleInner = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+    circleInner.setAttributeNS(null, 'cx', 15.25);
+    circleInner.setAttributeNS(null, 'cy', 18);
+    circleInner.setAttributeNS(null, 'r', 3.5);
+    circleInner.setAttributeNS(null, 'style', 'fill: white;');
+    buttonPath.appendChild(circleInner);
+}
+
+addButton();
